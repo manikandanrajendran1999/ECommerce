b58121a32b3b8a1028f988fcf856699448479a5c
Manikandan <manikandan@digivalsolutions.com>
2025-11-04 12:39:10 +0530

    Fix the hard error

--- src/test/java/org/baseUtils/Listener.java
+++ src/test/java/org/baseUtils/Listener.java
@@ -4,11 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-import org.testng.internal.annotations.IListeners;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
+import com.baseUtils.ExtendReporterNG;
 
 public class Listener extends Hooks implements ITestListener{
 	
